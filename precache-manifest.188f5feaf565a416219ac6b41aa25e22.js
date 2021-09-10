cc0b85409488256bfde7bf1931621e52bad46d1a
LoveLakhwani <86642913+lakhwaniji@users.noreply.github.com>
2021-09-10 09:31:09 +0530

    Updates

---
+++ precache-manifest.188f5feaf565a416219ac6b41aa25e22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2d9b2ca226380d8a541471e50f8cda",
+    "url": "/robofreinds-with-redux-1/index.html"
+  },
+  {
+    "revision": "c4f27f2e9b0a93e2efef",
+    "url": "/robofreinds-with-redux-1/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "79cd04500e1d77a979ef",
+    "url": "/robofreinds-with-redux-1/static/css/main.60555379.chunk.css"
+  },
+  {
+    "revision": "c4f27f2e9b0a93e2efef",
+    "url": "/robofreinds-with-redux-1/static/js/2.f0f2ef94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofreinds-with-redux-1/static/js/2.f0f2ef94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79cd04500e1d77a979ef",
+    "url": "/robofreinds-with-redux-1/static/js/main.65692377.chunk.js"
+  },
+  {
+    "revision": "cf8b040952a439423f97",
+    "url": "/robofreinds-with-redux-1/static/js/runtime-main.d1c54d9b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofreinds-with-redux-1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
